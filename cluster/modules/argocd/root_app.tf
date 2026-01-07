37a0ac0773f4722dd3e9aa3074b4402b63f6d882
tomas-rojo <tomasrojo21@hotmail.com>
2026-01-07 02:05:33 +0100

    Add argocd projects

---
+++ cluster/modules/argocd/root_app.tf
@@ -0,0 +1,50 @@
+resource "kubernetes_manifest" "root_app" {
+  depends_on = [ helm_release.argocd ]
+
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+
+    metadata = {
+      name      = "root-app"
+      namespace = "argocd"
+    }
+
+    spec = {
+      project = "default"
+
+      source = {
+        repoURL        = "https://github.com/tomas-rojo/homelab.git"
+        targetRevision = "HEAD"
+        path           = "gitops/appsets"
+      }
+
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = "argocd"
+      }
+
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
+        }
+
+        syncOptions = [
+          "CreateNamespace=true",
+          "Replace=true",
+        ]
+
+        retry = {
+          limit = 2
+
+          backoff = {
+            duration    = "5s"
+            factor      = 2
+            maxDuration = "3m0s"
+          }
+        }
+      }
+    }
+  }
+}
